0a24748109f65188ea1923ffaf92b9672d8941d2
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2017-06-08 19:59:27 +0300

    Fix OpenCV includes
    to build the lib with standard Ubuntu package 'libopencv-dev'

--- src/akaze_match.cpp
+++ src/akaze_match.cpp
@@ -21,8 +21,8 @@
 #include "./lib/AKAZE.h"
 
 // OpenCV
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
 
